df110282bfc764342ceb0f8fac63393f6402aa5b
hanna_varhan <hannavarhan@gmail.com>
2021-04-01 13:33:52 +0300

    update parser

--- src/main/java/com/epam/array/parser/StringParser.java
+++ src/main/java/com/epam/array/parser/StringParser.java
@@ -1,18 +1,17 @@
 package com.epam.array.parser;
 
-import com.epam.array.entity.ArrayEntity;
 import com.epam.array.exception.ArrayException;
 
 public class StringParser {
 
     final String SPACE_REGEX = "\\s+";
 
-    public ArrayEntity parse(String numberString) throws ArrayException { //return просто массив
+    public int[] parse(String numberString) throws ArrayException { //return просто массив
         String[] lineElements = numberString.split(SPACE_REGEX);
-        ArrayEntity arrayEntity = new ArrayEntity(lineElements.length);
+        int[] array = new int[lineElements.length];
         for (int i = 0; i < lineElements.length; i++) {
-            arrayEntity.set(i, Integer.parseInt(lineElements[i]));
+            array[i] = Integer.parseInt(lineElements[i]);
         }
-        return arrayEntity;
+        return array;
     }
 }
